68b4f328bed9d6df46d762ed254e9e47b51bda94
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-03-05 08:06:24 +0500

    UI updates

--- src/App.js
+++ src/App.js
@@ -5,10 +5,18 @@ import {Provider} from "react-redux";
 import store from "./store";
 
 function App() {
+
+
+
     return (
         <Provider store={store}>
+            <header className={'bg-primary'}>
+                <div className={'text-center text-white p-4'}>
+                    <h1>Post Tracker App</h1>
+                </div>
+            </header>
             <div className="container">
-                <div className={'m-4'}>
+                <div className={'m-5'}>
                     <PostForm/>
                 </div>
                 <div className={''}>
